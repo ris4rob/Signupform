e3e9442ec83cc0294850d80ef8c6991577078cc1
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-09-22 14:56:25 +0800

    Complete form validation

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const inputs = document.querySelectorAll('input');
 const errorMessages = document.querySelectorAll('.row-section p');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmPassword');
+const passwordError = document.querySelector('.password-error');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
 
 // Add a click event listener to the external button
 createAccountBtn.addEventListener('click', function (event) {
@@ -15,9 +18,33 @@ createAccountBtn.addEventListener('click', function (event) {
   inputs.forEach(function (input, index) {
     if (input.value.trim() === '') {
       errorMessages[index].classList.add('error-message');
+    } else {
+      errorMessages[index].classList.remove('error-message');
     }
   });
+
+  if (password.value !== confirmPassword.value) {
+    passwordError.innerText = '* Password does not match';
+    passwordError.classList.add('error-message');
+  }
+
+  if (!isValidEmail(email.value.trim())) {
+    // Prevent the default alert message from appearing.
+    event.preventDefault();
+    emailError.innerText = '* Looks like this is not an email';
+    emailError.classList.add('error-message');
+  } else {
+    emailError.classList.remove('error-message');
+  }
 
   // Submit the form programmatically
   signupForm.submit();
 });
+
+function isValidEmail(email) {
+  // Regular expression pattern to validate email format
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  return emailRegex.test(email);
+}
+
+('* Looks like this is not an email');

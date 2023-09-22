cdd5450a47f564ae483bfe4cbeaaa2e3b67212e4
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-09-22 13:38:29 +0800

    Add variables eventlistener and loop over inputs for form validation

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // Use external submit button with form
 // Get references to the form and the external button
-const signupForm = document.getElementById('signupForm');
-const createAccountBtn = document.getElementById('externalSubmitBtn');
+const signupForm = document.querySelector('.signupForm');
+const createAccountBtn = document.querySelector('#externalSubmitBtn');
+const inputs = document.querySelectorAll('input');
+const errorMessages = document.querySelectorAll('.row-section p');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
 
 // Add a click event listener to the external button
 createAccountBtn.addEventListener('click', function (event) {
   // Prevent the default form submission behavior
   event.preventDefault();
+
+  inputs.forEach(function (input, index) {
+    if (input.value.trim() === '') {
+      errorMessages[index].classList.add('error-message');
+    }
+  });
 
   // Submit the form programmatically
   signupForm.submit();
